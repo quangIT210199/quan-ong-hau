5c8bb9caebc44643250dbef55cefab566738b39e
quang <quang.itptit@gmail.com>
2021-04-08 13:16:09 +0700

    dang lam login va siginup

---
+++ src/main/java/com/codelovers/quanonghau/controller/RequestPojo/RequestPojo.java
@@ -0,0 +1,14 @@
+package com.codelovers.quanonghau.controller.RequestPojo;
+
+// Chưa dùng @@
+public class RequestPojo {
+    private int[] cartItemId;
+
+    public int[] getCartItemId() {
+        return cartItemId;
+    }
+
+    public void setCartItemId(int[] cartItemId) {
+        this.cartItemId = cartItemId;
+    }
+}
